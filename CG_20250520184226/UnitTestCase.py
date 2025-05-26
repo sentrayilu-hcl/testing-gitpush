94019f091e5f6cae01b8eb89b7b2fbc6ae89b426
sentrayilu-hcl <sentrayilu.d@hcltech.com>
2025-05-26 13:11:45 +0530

    CG_20250520184226

---
+++ CG_20250520184226/UnitTestCase.py
@@ -0,0 +1,8 @@
+# Disclaimer: This output contains AI-generated content; user is advised to review it before consumption.
+#*Start of AI Generated Content*
+
+<font color='red'>Security Issues Found.</font>
+
+<table><tr><th>Issue Type</th><th>Details</th></tr><tr><td> 'Anonymize'</td><td>Issues detected with anonymization</td></tr><tr><td> 'BanCode'</td><td>Issues detected with banned code</td></tr><tr><td> 'Gibberish'</td><td>Gibberish text detected</td></tr></table><div style='margin-top:5px'>The data provided is not safe and poses a security threat. Further processing is not possible, and the request has been terminated.</div><div style='height:10px'></div>
+
+#*End of AI Generated Content*
